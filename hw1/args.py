6acae40eba3318855cde56432e18c6d6a285e08a
kaikai4n <b04902131@csie.ntu.edu.tw>
2018-09-26 13:28:57 +0800

    Use argparse to parse args

---
+++ hw1/args.py
@@ -0,0 +1,28 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--validation',
+            action='store_true',
+            default=False,
+            help='To split validation or not.')
+    parser.add_argument('--train_filename',
+            default='data/train.csv',
+            help='The csv file to train')
+    parser.add_argument('--attributes_filename',
+            default='models/attributes_PM2.5_PM10.npy',
+            help='The filtered boolean numpy file,\
+                    specified which attributes are used \
+                    to train.')
+    parser.add_argument('-e', '--epoches',
+            default=2000)
+    parser.add_argument('-lr', '--learning_rate',
+            default=1e-3)
+    parser.add_argument('--save_intervals',
+            default=100,
+            help='The epoch intervals to save models')
+    parser.add_argument('--prefix',
+            required=True,
+            help='The prefix of saving name')
+    args = parser.parse_args()
+    return args
