bc7709251afe4a9389dbf07ce9d079fa13e322e8
kaikai4n <b04902131@csie.ntu.edu.tw>
2018-10-19 16:38:45 +0800

    Change data loading way .

--- hw2/data.py
+++ hw2/data.py
@@ -8,6 +8,21 @@ class DataProcessor():
         ones = np.ones((x.shape[0], 1))
         return np.concatenate([ones, x], 1)
 
+    def _normalize(self, x):
+        for i in range(15):
+            mean, std = np.mean(x[i]), np.std(x[i])
+            x[i] = (x[i] - mean)/std
+        return x
+
+    def _augment(self, x):
+        for i in range(15):
+            for exp_num in range (20):
+                aug_data = np.power(x[:, i], exp_num)
+                x = np.concatenate([x, aug_data.reshape(-1, 1)], axis=1)
+        return x
+
     def augment_features(self, train_x):
         train_x = self._add_bias(train_x)
+        #train_x = self._normalize(train_x)
+        #train_x = self._augment(train_x)
         return train_x
--- hw2/load.py
+++ hw2/load.py
@@ -1,23 +1,56 @@
 import pandas
+import numpy as np
 
 class DataLoader():
     def __init__(self):
         pass
-    
-    def read_training_data(self, train_x_filename, train_y_filename):
-        print(train_x_filename, train_y_filename)
+
+    def read_data(self, train_x_filename, train_y_filename, test_filename):
         train_x = self.read_csv(train_x_filename)
-        train_x = self.to_numpy(train_x)
         train_y = self.to_numpy(self.read_csv(train_y_filename))
+        test_x = self.read_csv(test_filename)
+        all_x = train_x.append(test_x, ignore_index=True)
+        all_x = self.data_processing(all_x)
+        all_x = self.to_numpy(all_x)
+        train_x, test_x = all_x[:20000], all_x[20000:]
+        return train_x, train_y, test_x
+
+    def read_training_data(self, train_x_filename, train_y_filename, test_filename):
+        train_x , train_y , _ = \
+                self.read_data(train_x_filename, train_y_filename, test_filename)
         return train_x, train_y
+
+    def read_testing_data(self, train_x_filename, train_y_filename, test_filename):
+        _ , _ , test_x = self.read_data(train_x_filename, train_y_filename, test_filename)
+        return test_x
+    
+    def data_processing(self, x):
+        #keys = ['EDUCATION']
+        x = self.filter_feature(x)
+        x = self.to_one_hot(x)
+        return x
 
     def read_csv(self, filename):
         content = pandas.read_csv(filename, dtype=float)
         return content
 
-    def filter_feature(self, content):
-        keys = content.keys()
-        content = content[keys]
+    def filter_feature(self, content, keys=None):
+        if keys is None:
+            # All keys are needed
+            return content
+        else:
+            content = content[keys]
+            return content
+
+    def to_one_hot(self, content):
+        keys = ['SEX', 'EDUCATION', 'MARRIAGE', \
+                'PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6']
+        for one_key in keys:
+            if one_key in content.keys():
+                this_content = content[one_key]
+                this_one_hot = pandas.get_dummies(this_content, prefix=one_key)
+                content = content.drop(columns=one_key)
+                content = pandas.concat([content, this_one_hot], axis=1)
         return content
 
     def to_numpy(self, content):
