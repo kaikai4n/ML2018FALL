eca25d6b4599ce19f601577163ecb80adc367f9a
kaikai4n <b04902131@csie.ntu.edu.tw>
2018-10-21 00:26:32 +0800

    Change some detail to fit the new optimizer.

--- hw2/train.py
+++ hw2/train.py
@@ -15,15 +15,26 @@ def train(
         learning_rate, 
         save_intervals, 
         prefix,
-        optim_name):
+        optim_name,
+        validation):
+    # Make sure the save pathes are valid
     if os.path.isdir('models') == False:
         os.mkdir('models')
+    save_model_prefix = os.path.join('models', prefix)
+    if os.path.isdir(save_model_prefix) == False:
+        os.mkdir(save_model_prefix)
+    
     feature_num = train_x.shape[1]
+    if validation:
+        data_processor = data.DataProcessor()
+        train_x, train_y, valid_x, valid_y = \
+                data_processor.cut_validation(train_x, train_y)
     total_data = train_x.shape[0]
     try:
         optim_object = getattr(optimizer, optim_name)
     except AttributeError:
         print('Optimizer not found.')
+        exit()
     optim = optim_object(learning_rate)
     trainer = model.LogisticRegression(
             feature_num,
@@ -42,10 +53,19 @@ def train(
             total_accuracy += trainer.count_accuracy(pred, y)
         total_loss /= total_data
         accuracy = total_accuracy * batch_size / total_data
-        print('epoch:%d, loss:%.3f, Ein:%.3f' \
-                % (epoch, total_loss, accuracy))
+        if validation:
+            valid_pred = trainer.forward(valid_x)
+            print(valid_pred)
+            valid_accuracy = trainer.count_accuracy(valid_pred, valid_y)
+            message = 'epoch:%3d, loss:%.3f, accuracy:%.3f, validation:%.3f'\
+                    % (epoch, total_loss, accuracy, valid_accuracy)
+        else:
+            message = 'epoch:%3d, loss:%.3f, accuracy:%.3f' % \
+                    (epoch, total_loss, accuracy)
+        print(message)
         if (epoch + 1) % save_intervals == 0:
-            save_model_path = os.path.join('models', '%s_e%d.npy' % (prefix, epoch+1))
+            save_model_path = os.path.join(
+                    save_model_prefix, 'model_e%d.npy' % (epoch+1))
             trainer.save_model(save_model_path)
 
 if __name__ == '__main__':
@@ -54,10 +74,6 @@ if __name__ == '__main__':
     data_loader = load.DataLoader()
     train_x, train_y = data_loader.read_training_data(
             args.train_x_filename, args.train_y_filename, args.test_x_filename)
-    print(train_x.shape, train_y.shape)
-    data_processor = data.DataProcessor()
-    train_x = data_processor.augment_features(train_x)
-    print(train_x.shape, train_y.shape)
     train(
             train_x, 
             train_y,
@@ -66,4 +82,5 @@ if __name__ == '__main__':
             args.learning_rate,
             args.save_intervals,
             args.prefix,
-            args.optimizer)
+            args.optimizer,
+            args.validation)
