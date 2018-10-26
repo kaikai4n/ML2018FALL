c34e95fac37cec4d739410b87b421616bbd3f85c
kaikai4n <b04902131@csie.ntu.edu.tw>
2018-10-27 00:11:41 +0800

    Add GaussianNaiveBayes.

--- hw2/model.py
+++ hw2/model.py
@@ -50,3 +50,77 @@ class LogisticRegression():
     def load_model(self, filename):
         with open(filename, 'rb') as f:
             self._weights = np.load(f)
+
+
+class GaussianNaiveBayes():
+    def __init__(self):
+        # The probabilistic generative model
+        # P(C1|X) = P(C1|X)P(C1) / (P(C1|X)P(C1) + P(C2|X)P(C2))
+        self._have_parameters = False
+
+    def fit(self, x, y, feature_num):
+        # x = [data_num, feature_dim]
+        # y = [data_num]
+        if self._have_parameters:
+            raise Exception('Check carefully \
+                    if you try to fit a new model. Previous \
+                    fitted model parameters will be replaced.')
+        self._labels = np.sort(np.unique(y))
+        self._dim = feature_num
+        self._prior = []
+        self._mus = []
+        self._sigmas = []
+        for one_label in self._labels:
+            label_indexes = (y == one_label).squeeze()
+            self._prior.append(np.sum(label_indexes)/y.shape[0])
+            self._mus.append(np.mean(x[label_indexes], axis=0))
+            self._sigmas.append(np.cov(x[label_indexes].T))
+        for i, (prior, sigma) in enumerate(zip(self._prior, self._sigmas)):
+            if i == 0:
+                self._sigma = prior * sigma
+            else:
+                self._sigma += prior * sigma
+        self._inv_sigma = np.linalg.pinv(self._sigma)
+        self._have_parameters = True
+
+    def predict(self, x):
+        if self._have_parameters == False:
+            raise Exception('Called predict before fit or load model.')
+            exit()
+        z = x.dot((self._mus[0] - self._mus[1]).T.dot(self._inv_sigma).T) - \
+                (1/2) * self._mus[0].T.dot(self._inv_sigma).dot(self._mus[0]) +\
+                (1/2) * self._mus[1].T.dot(self._inv_sigma).dot(self._mus[1]) +\
+                np.log(self._prior[0]/self._prior[1])
+        pred_prob = self._sigmoid(z)
+        pred = self._prob_to_label(pred_prob)
+        return pred
+
+    def _sigmoid(self, x):
+        # Prevent overflow
+        x[x < -1e2] = -1e2
+        x[x > 1e2] = 1e2
+        return 1.0/(1.0+np.exp(-x))
+
+    def _prob_to_label(self, prob):
+        class1_indexes = (prob >= 0.5)
+        class2_indexes = (prob < 0.5)
+        label = prob
+        label[class1_indexes] = self._labels[0]
+        label[class2_indexes] = self._labels[1]
+        return label
+
+    def count_accuracy(self, pred, y):
+        return np.sum(pred == y.squeeze()) / y.shape[0]
+    
+    def save_model(self, filename):
+        weights = (self._labels, self._prior, \
+                self._mus, self._sigma, self._inv_sigma)
+        with open(filename, 'wb') as f:
+            np.save(f, weights)
+
+    def load_model(self, filename):
+        with open(filename, 'rb') as f:
+            weights = np.load(f)
+        (self._labels, self._prior, self._mus, \
+                self._sigma, self._inv_sigma) = weights 
+        self._have_parameters = True
