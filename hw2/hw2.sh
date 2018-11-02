a2513824b1bc56a2f68304f00be1fb397437d68b
kaikai4n <b04902131@csie.ntu.edu.tw>
2018-11-02 18:32:07 +0800

    Fix parameter problems.

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -1,1 +1,1 @@
-python3 test.py --model_filename=models/model_logistic.npy --output=$4 --model=GaussianNaiveBayes --train_x_filename=$1 --train_y_filename=$2 --test_x_filename=$3
+python3 test.py --model_filename=models/model_logistic.npy --output=$4 --train_x_filename=$1 --train_y_filename=$2 --test_x_filename=$3
