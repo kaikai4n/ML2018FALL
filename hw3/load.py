d4243c2ad375656748d83af016bb7eba4d38ff5a
Yu-Kai Huang <kaikai4n@cml23.csie.ntu.edu.tw>
2018-11-12 21:33:33 +0800

    Wrap into a DataLoader class.

--- hw3/load.py
+++ hw3/load.py
@@ -1,21 +1,26 @@
 import pandas as pd
 import numpy as np
 
-def load_training_data(filename):
-    df_training = pd.read_csv(filename)
-    y = df_training['label'].values
-    x = df_training['feature'].tolist()
-    x = [[int(value) for value in ele.split()] for ele in x]
-    x = np.array(x)
-    return x, y
 
-def load_testing_data(filename):
-    df_training = pd.read_csv(filename)
-    x_id = df_training['id'].values
-    x = df_training['feature'].tolist()
-    x = [[int(value) for value in ele.split()] for ele in x]
-    x = np.array(x)
-    return x_id, x
+class DataLoader():
+    def __init__(self):
+        pass
+
+    def load_training_data(self, filename):
+        df_training = pd.read_csv(filename)
+        y = df_training['label'].values
+        x = df_training['feature'].tolist()
+        x = [[int(value) for value in ele.split()] for ele in x]
+        x = np.array(x, dtype=np.float32).reshape(-1, 1, 48, 48)
+        return x, y
+
+    def load_testing_data(self, filename):
+        df_training = pd.read_csv(filename)
+        x_id = df_training['id'].values
+        x = df_training['feature'].tolist()
+        x = [[int(value) for value in ele.split()] for ele in x]
+        x = np.array(x)
+        return x[:,1:]
 
 
 if __name__ == '__main__':
