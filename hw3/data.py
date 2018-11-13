9741c95eba0f7ac037c5a58ef9f1204fa304b083
Yu-Kai Huang <kaikai4n@cml23.csie.ntu.edu.tw>
2018-11-13 14:50:51 +0800

    Add DataPreprocessor with normalize to [0,1].

--- hw3/data.py
+++ hw3/data.py
@@ -13,3 +13,17 @@ class FaceDataset(Dataset):
 
     def __getitem__(self, i):
         return self.x[i], self.y[i]
+
+class DataProcessor():
+    def __init__(self, x, y, data_length):
+        self.x = x
+        self.y = y
+        self.data_length = data_length
+
+    def normalize(self):
+        self.x = self.x / 255
+
+    def get_data(self):
+        return self.x, self.y
+
+
