410cca0314a9b749a12bdeecde8728ab86d81d8c
Yu-Kai Huang <kaikai4n@cml23.csie.ntu.edu.tw>
2018-11-13 16:50:55 +0800

    Add cut validation.

--- hw3/data.py
+++ hw3/data.py
@@ -1,5 +1,5 @@
 from torch.utils.data import Dataset
-
+import numpy as np
 
 class FaceDataset(Dataset):
     def __init__(self, x, y, data_length):
@@ -16,14 +16,24 @@ class FaceDataset(Dataset):
 
 class DataProcessor():
     def __init__(self, x, y, data_length):
-        self.x = x
-        self.y = y
-        self.data_length = data_length
+        self._x = x
+        self._y = y
+        self._data_length = data_length
+        self._validation = False
 
     def normalize(self):
-        self.x = self.x / 255
+        self._x = self._x / 255
 
     def get_data(self):
-        return self.x, self.y
+        if self._validation:
+            return self._x[self._permut][:self._train_num],\
+                    self._y[self._permut][:self._train_num],\
+                    self._x[self._permut][self._train_num:],\
+                    self._y[self._permut][self._train_num:]
+        else:
+            return self._x, self._y, None, None
 
-
+    def cut_validation(self):
+        self._validation = True
+        self._train_num = int(self._data_length * 0.9)
+        self._permut = np.random.permutation(self._data_length)
